2457b8dbda2a567b4f68626884b5ef2f08eac886
MaximoLF <maximolorcafont@gmail.com>
2026-04-24 16:47:05 -0400

    Tarea

--- trabajo con String.py
+++ trabajo con String.py
@@ -44,9 +44,36 @@
 
 # CREAR UN PIN Y QUE TENGA EXACTAMENTE 4 DIGITOS
 
-pin=int(input("Ingrese su pin: "))
-if len(str(pin))==4:
-    print("Pin creado")
-else:
-    print("El pin debe ser de 4 digitos")
+# pin=int(input("Ingrese su pin: "))
+# if len(str(pin))==4:
+#     print("Pin creado")
+# else:
+#     print("El pin debe ser de 4 digitos")
 
+#----------------------------------------------------------------------------------------------------------------------------
+
+
+# Definir 2 candidatos. Preguntar la cantidad de votantes y preguntar a cada votante por quien votara mostrando las alternativas.
+# Contar los votos y mostrar resultador. Definir el ganador y considerar un empate
+
+toon1=input("Ingrese el Toon1: ")
+toon2=input("Ingrese el Toon2: ")
+v1=0
+v2=0
+num=int(input("Ingrese la cantidad de votantes: "))
+
+for i in range(num):
+    voto=int(input(f"Por quien votara. 1.-{toon1} 2.-{toon2} "))
+    if voto==1:
+        v1+=1
+    elif voto==2:
+        v2+=1
+    else:
+        print("Voto no valido")
+
+    if v1>v2:
+        print(f"El ganador es {toon1} con {v1} votos")
+    elif v1<v2:
+        print(f"El ganador es {toon2} con {v2} votos")
+    else:
+        print("Es un empate")
